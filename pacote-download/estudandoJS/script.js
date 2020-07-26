73b1e164ef01d9202447fcd44b281a3862073526
Lucas Rocha <wltopos@gmail.com>
2020-07-25 21:29:23 -0300

    Treinando condições simples

---
+++ pacote-download/estudandoJS/script.js
@@ -0,0 +1,8 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //condição simples não tem 'else if'
+    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}else{
+    console.log(`Dirija sempre usando cinto de segurança`)
+}
+
