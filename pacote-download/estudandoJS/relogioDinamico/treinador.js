57b80f406770c25d5acf80367d5fa3cf53c73c01
Lucas Rocha <wltopos@gmail.com>
2020-07-26 17:21:11 -0300

    Fix

--- pacote-download/estudandoJS/relogioDinamico/treinador.js
+++ pacote-download/estudandoJS/relogioDinamico/treinador.js
@@ -11,25 +11,25 @@ if(idHora < 6){
     console.log(`Boa madrugada agora são exatamente: ${idHora}:${idMinuto}h`)
     //boa madrugada
     txtHora.innerHTML = `Boa madrugada agora são exatamente: ${idHora}:${idMinuto}h`
-    imgHora.src = "imagens/compactadas/madrugada-min.png"
+    imgHora.src = "imagens/relogioDinamico/madrugada-min.png"
     bgHora.style.background = "#00061c"
 } else if(idHora < 12){
     console.log(`Bom dia agora são exatamente: ${idHora}:${idMinuto}`)
     //bom dia
     txtHora.innerHTML = `Bom dia agora são exatamente: ${idHora}:${idMinuto}h`
-    imgHora.src = "imagens/compactadas/dia-min.png"
+    imgHora.src = "imagens/relogioDinamico/dia-min.png"
     bgHora.style.background = "rgb(218, 238, 242)"
 } else if(idHora < 16){
     console.log(`Boa tarde agora são exatamente: ${idHora}:${idMinuto}`)
     //boa tarde
     txtHora.innerHTML = `Boa tarde agora são exatamente: ${idHora}:${idMinuto}h`
-    imgHora.src = "imagens/compactadas/tarde-min.png"
+    imgHora.src = "imagens/relogioDinamico/tarde-min.png"
     bgHora.style.background = "rgb(192, 82, 10)"
 } else if(idHora < 24){
     console.log(`Boa noite agora são exatamente: ${idHora}:${idMinuto}`)
     //Boa noite
     txtHora.innerHTML = `Boa noite agora são exatamente: ${idHora}:${idMinuto}h`
-    imgHora.src = "imagens/compactadas/noite_-min.png"
+    imgHora.src = "imagens/relogioDinamico/noite_-min.png"
     bgHora.style.background = "rgb(11, 53, 61)"
 } else{
     txtHora.innerHTML = `[ERRO: ${idHora}] Desculpe, não conseguimos identificar a hora correta :(`
