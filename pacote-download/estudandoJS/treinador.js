c15efbd4ee236e64ce2a4381788427011ff19ddd
Lucas Rocha <wltopos@gmail.com>
2020-07-26 11:25:09 -0300

    Adicionado recursos ao Verificador de horas - Treinador

--- pacote-download/estudandoJS/treinador.js
+++ pacote-download/estudandoJS/treinador.js
@@ -1,10 +1,12 @@
 // Verificador de horas
 var hora = 19
 console.log(`Agora são exatamente ${hora}:00 horas.`)
-if(hora < 12){
-    console.log(`Dia`)
+if(hora < 6){
+    console.log(`Boa Madrugada :)`)
+} else if(hora < 12){
+    console.log(`Bom Dia :|`)
 } else if(hora <= 16){
-    console.log(`Tarde`)
+    console.log(`Boa Tarde ;)`)
 } else {
-    console.log(`Noite`)
+    console.log(`Boa Noite (^_^)`)
 }
