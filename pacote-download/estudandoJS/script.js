5b48e6c98756faa73b5e47142a48768cf014e9ce
Lucas Rocha <wltopos@gmail.com>
2020-07-25 21:35:49 -0300

    Trabalhando com condições compostas

--- pacote-download/estudandoJS/script.js
+++ pacote-download/estudandoJS/script.js
@@ -1,8 +1,9 @@
-var vel = 50.5
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if(vel > 60){ //condição simples não tem 'else if'
-    console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
-}else{
-    console.log(`Dirija sempre usando cinto de segurança`)
+var pais = 'Brasi'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil')// Trabalhando com condições compostas 'if else'
+{
+    console.log('Você é um Brasileiro')
+} else {
+    console.log('Você é um Estrangeiro')
 }
 
